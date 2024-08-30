955afb0c96531ccf95c0eac8d395f38d7e8eca3b
Huynh Duong <huynh.dh2310@gmail.com>
2024-08-30 21:37:58 +0700

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,6 +14,28 @@ return {
         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
         desc = "Find Plugin File",
       },
+      {
+        "<leader>fb",
+        function()
+          local telescope = require("telescope")
+
+          local function telescope_buffer_dir()
+            return vim.fn.expand("%:p:h")
+          end
+
+          telescope.extensions.file_browser.file_browser({
+            path = "%:p:h",
+            cwd = telescope_buffer_dir(),
+            respect_gitignore = false,
+            hidden = true,
+            grouped = true,
+            previewer = false,
+            initial_mode = "normal",
+            layout_config = { height = 40 },
+          })
+        end,
+        desc = "Open File Browser with the path of the current buffer",
+      },
     },
     -- change some options
     opts = {
