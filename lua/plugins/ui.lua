7ddecfbbf423472f04a4227acccce187f214e31d
Huynh Duong <huynh.dh2310@gmail.com>
2024-10-21 00:39:20 +0700

    Add files via upload

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -31,7 +31,7 @@ return {
 
       local opts = {
         options = {
-          theme = "catppuccin",
+          theme = "auto",
           globalstatus = vim.o.laststatus == 3,
           disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter" } },
           section_separators = "",
